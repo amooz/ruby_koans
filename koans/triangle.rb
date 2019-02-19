936fc081ca10ab80d0f8588d4d4206014a5b7ff4
Adam Mooz <Adam.Mooz@Shopify.com>
2019-02-18 20:08:08 -0500

    about_triagnle_project_2

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,16 +14,17 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
-  if(a === b && b == c) then
-    return :equilateral
-  end
+  values = [a, b, c]
+  uniqueValues = values.uniq
+  sortedValues = values.sort
+  
+  raise TriangleError unless values.all? {|value| value > 0}
+  raise TriangleError if uniqueValues.length === 2  && sortedValues[2] >= sortedValues[0] + sortedValues[1]
 
-  if(a ===b || b === c || a === c) then
-    return :isosceles
-  end
-
+  return :equilateral if uniqueValues.length === 1
+  return :isosceles if uniqueValues.length === 2
   return :scalene
+
 end
 
 # Error class used in part 2.  No need to change this code.
